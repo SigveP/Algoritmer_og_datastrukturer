2e2f2671e54013f4693ce1877ff0c70e68f9ea1b
Lavhran <lavhran@gmail.com>
2021-11-25 12:42:54 +0100

    Added more example files

---
+++ eks_hashtable.py
@@ -0,0 +1,142 @@
+from datastrukturer import HashTable
+from enum import Enum
+import tkinter as tk
+
+
+def load_data(file: str) -> HashTable:
+    ht = HashTable()
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            for entry in f:
+                entry = entry.split(':')
+                ht.add(entry[0], entry[1][:-1])
+        return ht
+    except:
+        return ht
+
+
+def save(file, ht: HashTable) -> None:
+    with open(file, 'w', encoding='utf-8') as f:
+        for k, v in ht:
+            f.write(f'{k}:{v}\n')
+
+
+class GetType(Enum):
+    add = 0
+    update = 1
+
+
+class Window(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.title('eks_hashtable')
+        self.minsize(300, 100)
+
+        self.hashtable = load_data('eks_data/eks_hashtable_data')
+
+        self.buttona = tk.Button()  # add
+        self.buttonb = tk.Button()  # update value
+        self.buttonc = tk.Button()  # save
+        self.listbox = tk.Listbox()
+        self.scrollbar = tk.Scrollbar()
+
+        self.buttona.configure(text='Add', command=self.add_ht)
+        self.buttonb.configure(text='Change', command=self.update_ht)
+        self.buttonc.configure(text='Save', command=lambda: save(
+            'eks_data/eks_hashtable_data', self.hashtable))
+        self.listbox.configure(width=60, yscrollcommand=self.scrollbar.set)
+        self.scrollbar.configure(command=self.listbox.yview)
+
+        self.buttona.grid(column=0, row=0, sticky=(tk.W, tk.E))
+        self.buttonb.grid(column=1, row=0, sticky=(tk.W, tk.E))
+        self.buttonc.grid(column=2, row=0, sticky=(tk.W, tk.E))
+        self.listbox.grid(column=0, row=1, columnspan=3)
+        self.scrollbar.grid(column=3, row=0, rowspan=2, sticky=(tk.N, tk.S))
+
+        self.update_listbox()
+
+    def update_listbox(self) -> None:
+        self.listbox.delete('0', tk.END)
+        for i in self.sort(self.hashtable):
+            self.listbox.insert(tk.END, i)
+
+    def sort(self, ht: list, recursed: int = 0) -> list:
+        if isinstance(ht, HashTable):
+            temp = []
+            for k, v in ht:
+                temp.append(f'{k} - {v}')
+            ht = temp
+        values = "0123456789abcdefghijklmnopqrstuvwxyzæøå"
+        groups = [[] for i in range(len(values))]
+        for i in ht:
+            for j in values:
+                if len(i) <= recursed:
+                    break
+                elif i[recursed].lower() == j:
+                    groups[values.index(j)].append(i)
+                    break
+        for group in groups:
+            for i in group:
+                ht.remove(i)
+            if len(group):
+                ht += self.sort(group, recursed + 1)
+        return ht
+
+    def add_ht(self) -> None:
+        GetWindow(self, 'Key', GetType.add)
+
+    def update_ht(self) -> None:
+        value = self.listbox.get(self.listbox.curselection()).split(' - ')[0]
+        GetWindow(self, value, GetType.update)
+
+
+class GetWindow(tk.Tk):
+    def __init__(self, parent: Window, label: str, action: GetType) -> None:
+        super().__init__()
+
+        self.title(label)
+        self.parent = parent
+
+        if action == GetType.add:
+            self.keylabel = tk.Label(self, text=f'{label}: ')
+            self.keyentry = tk.Entry(self)
+        elif action == GetType.update:
+            self.keylabel = tk.Label(self, text=label)
+            self.label = label
+
+        self.valuelabel = tk.Label(self, text=f'Value: ')
+        self.valueentry = tk.Entry(self)
+
+        if action == GetType.add:
+            self.confirmbutton = tk.Button(
+                self, text='Confirm', command=self.add)
+        elif action == GetType.update:
+            self.confirmbutton = tk.Button(
+                self, text='Confirm', command=self.update)
+
+        if action == GetType.add:
+            self.keylabel.grid(column=0, row=0)
+            self.keyentry.grid(column=1, row=0)
+        elif action == GetType.update:
+            self.keylabel.grid(column=1, row=0)
+
+        self.valuelabel.grid(column=0, row=1)
+        self.valueentry.grid(column=1, row=1)
+
+        self.confirmbutton.grid(
+            column=0, row=2, columnspan=2, sticky=(tk.W, tk.E))
+
+    def add(self) -> None:
+        self.parent.hashtable.add(self.keyentry.get(), self.valueentry.get())
+        self.parent.update_listbox()
+        self.destroy()
+
+    def update(self) -> None:
+        self.parent.hashtable[self.label] = self.valueentry.get()
+        self.parent.update_listbox()
+        self.destroy()
+
+
+root = Window()
+root.mainloop()
--- eks_queue.py
+++ eks_queue.py
@@ -1,30 +1,43 @@
 from datastrukturer import Queue
 from os import system
 from time import sleep
+from functools import partial
 from random import randint
+import keyboard
+
+
+class Food:
+    def __init__(self) -> None:
+        self.new_pos()
+
+    def new_pos(self):
+        self.pos = (randint(0, 19), randint(0, 19))
 
 
 class Worm:
-    def __init__(self, length: int) -> None:
+    def __init__(self, length: int, food: Food) -> None:
         self.worm = Queue(*[(0, i) for i in range(length)])
         self.head = (0, length-1)
-        self.length = length
         self.dir = 0
+        self.food = food
 
     def move(self) -> None:
         if self.dir == 0:
             new_head = ((self.head[0]+1) % 20, self.head[1])
         elif self.dir == 1:
             new_head = (self.head[0], (self.head[1]+1) % 20)
-        elif self.dir == 3:
+        elif self.dir == 2:
             new_head = ((self.head[0]-1) % 20, self.head[1])
         else:
             new_head = (self.head[0], (self.head[1]-1) % 20)
         self.head = new_head
         self.worm.enqueue(new_head)
-        self.worm.dequeue()
+        if not new_head == self.food.pos:
+            self.worm.dequeue()
+        else:
+            self.food.new_pos()
 
-    def change_dir(self, r: bool) -> None:
+    def change_dir(self, r: bool, *args) -> None:
         if r:
             self.dir += 1
         else:
@@ -32,19 +45,25 @@ class Worm:
         self.dir %= 4
 
 
-def printscreen(blocked: list) -> None:
+def printscreen(blocked: list, food: tuple) -> None:
     visible = [['  ' for j in range(20)] for i in range(20)]
+    visible[food.pos[1]][food.pos[0]] = '▒▒'
     for i in blocked:
-        visible[i[1]][i[0]] = '▒▒'
-    for i in visible:
-        print(''.join(i))
-
-
-w = Worm(10)
-for i in range(32000):
+        visible[i[1]][i[0]] = '██'
     system('cls')
-    if not randint(0, 9):
-        w.change_dir((True, False)[randint(0, 1)])
+    for i in visible:
+        print('|' + ''.join(i) + '|')
+
+
+f = Food()
+w = Worm(5, f)
+
+keyboard.on_release_key('left', partial(w.change_dir, False))
+keyboard.on_release_key('right', partial(w.change_dir, True))
+
+system('color 73')
+
+for i in range(32000):
     w.move()
-    printscreen(w.worm)
-    sleep(0.032)
+    printscreen(w.worm, f)
+    sleep(0.064)
